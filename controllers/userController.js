aa746f0e58caf74204eb55705f56988649455e9c
Prasoon Soni <prasoonsoni03@gmail.com>
2022-11-03 23:22:19 +0530

    Changed Login Logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,20 +9,20 @@ const createUser = async (req, res) => {
         const { password } = req.body
         const salt = await bcrypt.genSalt(10)
         const hashPassword = await bcrypt.hash(password, salt)
+        const user = await User.create({
+            passwords: [],
+            credit_cards: []
+        })
         const data = {
             user: {
+                id: user._id,
                 password: hashPassword,
                 created_at: Date.now()
             }
         }
-        const token = jwt.sign(data, process.env.JWT_SECRET_KEY)
-        const user = await User.create({
-            token: token,
-            passwords: [],
-            credit_cards: []
-        })
-        if(!user) {
-            return res.json({success:false, message:"Error creating user"})
+        const token = jwt.sign(data, password)
+        if (!user) {
+            return res.json({ success: false, message: "Error creating user" })
         }
         res.json({ success: true, message: "Account created successfully", token: token })
     } catch (error) {
@@ -34,16 +34,21 @@ const createUser = async (req, res) => {
 const loginUser = async (req, res) => {
     try {
         const { password, token } = req.body
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+        const decoded = jwt.verify(token, password)
         const { user } = decoded
+        console.log(user.id)
         const isMatch = await bcrypt.compare(password, user.password)
         if (!isMatch) {
             return res.json({ success: false, message: "Invalid Credentials." })
         }
+        const userPresent = await User.findOne({ _id: user.id })
+        if (!userPresent) {
+            return res.json({ success: false, message: "User not found" })
+        }
         res.json({ success: true, message: "Login Successful", token: token })
     } catch (error) {
         console.log(error.message)
-        res.json({ success: false, message: "Token is not valid" })
+        res.json({ success: false, message: "Token or Password is not valid" })
     }
 }
 
--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose'
 const {Schema} = mongoose
 
 const UserSchema = new Schema({
-    token: { type: String, required: true },
     passwords: { type: [String], required: true, default: [] },
     credit_cards: { type: [String], required: true, default: [] }
 })
